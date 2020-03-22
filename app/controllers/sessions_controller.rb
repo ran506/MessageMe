d5aa0264852f9f58476aa113dccb9e67b6ec6b36
Ran Chen <ranchen27056@gmail.com>
2020-03-22 11:47:50 -0400

    restrict login and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:new, :create]
     def new
     end
     def create
@@ -17,5 +18,12 @@ class SessionsController < ApplicationController
         session[:user_id] =nil
         flash[:success] ="You have successfully logged out"
         redirect_to root_path
+    end
+    private
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "You haved loggeg in"
+            redirect_to root_path
+        end
     end
 end 
