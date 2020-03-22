68df0ae7eaf093368761aaba1680080d943e5aae
Ran Chen <ranchen27056@gmail.com>
2020-03-22 17:17:53 -0400

    Modify display

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,11 +3,14 @@ class MessagesController < ApplicationController
     def create
         message= current_user.messages.build(messages_params)
         if message.save
-        redirect_to root_path
+        ActionCable.server.broadcast "chatroom_channel", mod_message: message_render(message)
         end
     end
     private
     def messages_params
         params.require(:message).permit(:body)
+    end
+    def message_render(message)
+        render(partial: 'message', locals: {message: message})
     end
 end
